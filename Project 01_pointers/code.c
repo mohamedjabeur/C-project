017a7794848bab90fab193e89f6a2cd4011deec6
mohamedjabeur <mohamed.jabeur2004@gmail.com>
2026-08-08 16:39:40 +0200

    Update code.c

--- Project 01_pointers/code.c
+++ Project 01_pointers/code.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+void change_value(int *value)
+{
+    *value = 100;
+}
+
+int main(void)
+{
+    int x = 10;
+
+    printf("Before: %d\n", x);
+
+    change_value(&x);
+
+    printf("After: %d\n", x);
+
+    return 0;
+}
 
